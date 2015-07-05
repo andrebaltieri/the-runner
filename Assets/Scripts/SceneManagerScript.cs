2c1052e21fc554de8ddff9c4302b4add30334a69
Andre Baltieri <andrebaltieri@hotmail.com>
2015-07-05 13:29:30 -0300

    Adicionado arquivos

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManagerScript : MonoBehaviour {
+	// Update is called once per frame
+	public void LoadMainScene()
+	{
+		Application.LoadLevel("Scene");
+	}
+
+	public void LoadMainMenu()
+	{
+		Application.LoadLevel("MainMenu");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
